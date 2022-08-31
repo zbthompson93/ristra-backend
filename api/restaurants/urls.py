f43ccdfac10eb6b59ec3cd402b9003001104b88c
zbthompson93 <zbthompson93@penguin>
2022-08-31 16:12:33 -0400

    Update requests format

--- api/restaurants/urls.py
+++ api/restaurants/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from restaurants import views
 
 urlpatterns = [
     path('restaurants/', views.restaurant_list),
     path('restaurants/<int:pk>/', views.restaurant_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- api/restaurants/views.py
+++ api/restaurants/views.py
@@ -1,49 +1,48 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from restaurants.models import Restaurant
 from restaurants.serializers import RestaurantSerializer
 
-@csrf_exempt
-def restaurant_list(request):
+@api_view(['GET', 'POST'])
+def restaurant_list(request, format=None):
     """
     List all code restaurants, or create a new restaurant.
     """
     if request.method == 'GET':
         restaurants = Restaurant.objects.all()
         serializer = RestaurantSerializer(restaurants, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = RestaurantSerializer(data=data)
+        serializer = RestaurantSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
-def restaurant_detail(request, pk):
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def restaurant_detail(request, pk, format=None):
     """
     Retrieve, update or delete a code restaurant.
     """
     try:
         restaurant = Restaurant.objects.get(pk=pk)
     except Restaurant.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = RestaurantSerializer(restaurant)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = RestaurantSerializer(restaurant, data=data)
+        serializer = RestaurantSerializer(restaurant, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         restaurant.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
